863ced9aaf1564f7aecf76f4cce864073674bc25
Eugene Nosko <coder@venux.com>
2017-06-03 21:34:23 -0400

    PDF support

--- src/pdf/util.js
+++ src/pdf/util.js
@@ -1,26 +1,19 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
-
-// Copyright (C) 2015 the V8 project authors. All rights reserved.
-// This code is governed by the BSD license found in the LICENSE file.
-/**
- * Verify that the given date object's Number representation describes the
- * correct number of milliseconds since the Unix epoch relative to the local
- * time zone (as interpreted at the specified date).
- *
- * @param {Date} date
- * @param {Number} expectedMs
- */
-function assertRelativeDateMs(date, expectedMs) {
-  var actualMs = date.valueOf();
-  var localOffset = date.getTimezoneOffset() * 60000;
-
-  if (actualMs - localOffset !== expectedMs) {
-    $ERROR(
-      'Expected ' + date + ' to be ' + expectedMs +
-      ' milliseconds from the Unix epoch'
-    );
-  }
+// this will show what ends up in the fstream-npm package
+var P = require('fstream-ignore')
+var tar = require('tar')
+function f (entry) {
+  return entry.basename !== '.git'
 }
+
+new P({ path: './', type: 'Directory', Directory: true, filter: f })
+  .on('package', function (p) {
+    console.error('package', p)
+  })
+  .on('ignoreFile', function (e) {
+    console.error('ignoreFile', e)
+  })
+  .on('entry', function (e) {
+    console.error(e.constructor.name, e.path.substr(e.root.path.length + 1))
+  })
+  .pipe(tar.Pack())
+  .pipe(process.stdout)
