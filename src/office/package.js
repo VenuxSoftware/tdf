0a58473d90faa6bef14d47b98446dba896601c07
Eugene Nosko <coder@venux.com>
2018-01-10 00:13:04 -0500

    Office

--- src/office/package.js
+++ src/office/package.js
@@ -1,13 +1,84 @@
-// this example will bundle every dependency
-var P = require("../")
-P({ path: "./" })
-  .on("package", bundleIt)
-  .on("entry", function (e) {
-    console.error(e.constructor.name, e.path.substr(e.root.dirname.length + 1))
-    e.on("package", bundleIt)
-  })
+'use strict'
+var fs = require('graceful-fs')
+var path = require('path')
 
-function bundleIt (p) {
-  p.bundleDependencies = Object.keys(p.dependencies || {})
+var mkdirp = require('mkdirp')
+var osenv = require('osenv')
+var requireInject = require('require-inject')
+var rimraf = require('rimraf')
+var test = require('tap').test
+
+var common = require('../common-tap.js')
+
+var pkg = path.resolve(__dirname, 'gist-shortcut-package')
+
+var json = {
+  name: 'gist-shortcut-package',
+  version: '0.0.0',
+  dependencies: {
+    'private-gist': 'gist:foo/deadbeef'
+  }
 }
 
+test('setup', function (t) {
+  setup()
+  t.end()
+})
+
+test('gist-shortcut-package', function (t) {
+  var cloneUrls = [
+    ['git://gist.github.com/deadbeef.git', 'GitHub gist shortcuts try git URLs first'],
+    ['https://gist.github.com/deadbeef.git', 'GitHub gist shortcuts try HTTPS URLs second'],
+    ['ssh://git@gist.github.com/deadbeef.git', 'GitHub gist shortcuts try SSH third']
+  ]
+  var npm = requireInject.installGlobally('../../lib/npm.js', {
+    'child_process': {
+      'execFile': function (cmd, args, options, cb) {
+        process.nextTick(function () {
+          if (args.indexOf('clone') === -1) return cb(null, '', '')
+          var cloneUrl = cloneUrls.shift()
+          if (cloneUrl) {
+            t.is(args[args.length - 2], cloneUrl[0], cloneUrl[1])
+          } else {
+            t.fail('too many attempts to clone')
+          }
+          cb(new Error())
+        })
+      }
+    }
+  })
+
+  var opts = {
+    cache: path.resolve(pkg, 'cache'),
+    prefix: pkg,
+    registry: common.registry,
+    loglevel: 'silent'
+  }
+  npm.load(opts, function (er) {
+    t.ifError(er, 'npm loaded without error')
+    npm.commands.install([], function (er, result) {
+      t.ok(er, 'mocked install failed as expected')
+      t.end()
+    })
+  })
+})
+
+test('cleanup', function (t) {
+  cleanup()
+  t.end()
+})
+
+function setup () {
+  cleanup()
+  mkdirp.sync(pkg)
+  fs.writeFileSync(
+    path.join(pkg, 'package.json'),
+    JSON.stringify(json, null, 2)
+  )
+  process.chdir(pkg)
+}
+
+function cleanup () {
+  process.chdir(osenv.tmpdir())
+  rimraf.sync(pkg)
+}
